8b423ad66056af36e84b3c101f2708bd9a075d29
sunilmsanand <73874582+sunilmsanand@users.noreply.github.com>
2023-06-15 18:28:07 +0530

    Create install-python3.sh

---
+++ install-python3.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+  sudo apt update
+
+  sudo apt install python3
+ 
+  python3 -V
+  
+  sudo apt install -y python3-pip
+  
+  python --version
+  
+  #If the above install command doesn't work, please use this command
+  sudo apt-get install python3
+  #the following command displays the version of Python
+  python3
